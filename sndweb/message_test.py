a59c1f6a8f0ec6e925d8dac69e6ec3390675fbe3
Matthias Hannig <matthias@hannig.cc>
2018-01-24 17:39:47 +0100

    added tests for string string decoding

--- sndweb/message_test.py
+++ sndweb/message_test.py
@@ -4,37 +4,37 @@ import pytest
 from sndweb import message
 
 
-def test_encode_message_body():
+def test_encode_body():
 
     # No substitutions
     body = b'\x23\x42\x00\xff'
     expected = b'\x23\x42\x00\xff\x9e'
-    assert message.encode_message_body(body) == expected
+    assert message.encode_body(body) == expected
 
     body = b'\x23\x02\x03\xff'
     expected = b'\x23\x1b\x82\x1b\x83\xff\xdd'
-    assert message.encode_message_body(body) == expected
+    assert message.encode_body(body) == expected
 
 
-def test_decode_message_body():
+def test_decode_body():
     # Without substitutions
     body = b'\x23\x42\x00\xff'
-    encoded = message.encode_message_body(body)
-    assert body == message.decode_message_body(encoded)
+    encoded = message.encode_body(body)
+    assert body == message.decode_body(encoded)
 
     # With escaped chars
     body = b'\xfa\x02\x03\xff'
-    encoded = message.encode_message_body(body)
-    assert body == message.decode_message_body(encoded)
+    encoded = message.encode_body(body)
+    assert body == message.decode_body(encoded)
 
     # Trigger errors
     with pytest.raises(message.MessageError):
-        message.decode_message_body(b'f')
+        message.decode_body(b'f')
 
     with pytest.raises(message.ChecksumError):
         body = b'\xfa\x02\x03\xff'
-        encoded = b'\xff' + message.encode_message_body(body)
-        message.decode_message_body(encoded)
+        encoded = b'\xff' + message.encode_body(body)
+        message.decode_body(encoded)
 
 
 
@@ -67,11 +67,21 @@ def test_set_string():
 
 
 def test_decode_message():
+    # Set value
     body = b'\x80\x05\x02\x00\xf0'
     msg = message.decode_message(body)
 
     assert msg["type"] == message.SET_VALUE
     assert msg["payload"]["group"] == message.SW_AMX_LEVEL
-    assert msg["payload"]["control"] == 2
+    assert msg["payload"]["id"] == 2
     assert msg["payload"]["value"] == 0xf0
+
+    # Set string
+    body = b'\x81\x07\x23\x74\x65\x73\x74\x00'
+    msg = message.decode_message(body)
+
+    assert msg["type"] == message.SET_STRING
+    assert msg["payload"]["group"] == message.SW_AMX_TEXT
+    assert msg["payload"]["id"] == 0x23
+    assert msg["payload"]["value"] == "test"
 

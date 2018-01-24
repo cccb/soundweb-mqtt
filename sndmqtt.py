0c80aff2f6be8d2894908bba64cd0e6fab0ad0f8
Matthias Hannig <matthias@hannig.cc>
2018-01-24 11:44:04 +0100

    added cli parsing and logging setup

--- sndmqtt.py
+++ sndmqtt.py
@@ -1,10 +1,51 @@
 
+import argparse
+import logging
+
 from sndweb import connection, message
 from mqtt import service, actions
 
 
+
+def parse_args():
+    """Parse CLI arguments"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-b", "--broker",
+                        required=True,
+                        help="The MQTT broker host and port")
+    parser.add_argument("-t", "--topic",
+                        default="soundweb")
+    parser.add_argument("-s", "--serial",
+                        required=True,
+                        help="The serialdevice the soundweb is connected to")
+    parser.add_argument("-l", "--log-level",
+                        default="info",
+                        help="Set log level")
+
+    return parser.parse_args()
+
+
+def _setup_logging(level):
+    """Setup logger based on cli args"""
+    fmt = '%(asctime)s [%(name)s::%(levelname)s] %(message)s'
+    logging.basicConfig(format=fmt)
+
+    try:
+        log_level = getattr(logging, level.upper())
+    except:
+        logging.warning("Unknown log level ({}), falling back to INFO".format(
+            level))
+        log_level = logging.INFO
+
+    root = logging.getLogger()
+    root.setLevel(log_level)
+
+    logging.debug("initialized logging")
+
+
 def _handle_message(dispatch, message):
     pass
+
 
 def _handle_action(dispatch, send, action):
     if action["type"] == actions.SET_LEVEL_REQUEST:
@@ -18,15 +59,22 @@ def _handle_action(dispatch, send, action):
 
 def main(args):
     """Soundweb to MQTT bridge"""
+    # Setup logging
+    _setup_logging(args.log_level)
+
+    return
+
     # Connect to soundweb
     receive, send = connection.connect("/dev/tty.usbserial")
 
     # Connect to MQTT broker
     receive_action, dispatch = service.connect("localhost", "fnord")
 
-    print("Soundweb to MQTT ready")
+    # Show welcome message
+    print("Soundweb to MQTT                  v.0.1.0")
 
 
+    # Main loop
     for msg in receive():
         # Check for incoming serial data
         if msg:
@@ -47,6 +95,6 @@ def main(args):
 
 
 if __name__ == "__main__":
-    main({})
-
+    args = parse_args()
+    main(args)
 

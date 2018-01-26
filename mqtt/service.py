06826c521899edbb3444176503067bd32bfca597
Matthias Hannig <matthias@hannig.cc>
2018-01-26 16:37:31 +0100

    some cleaning up

--- mqtt/service.py
+++ mqtt/service.py
@@ -7,55 +7,45 @@ import time
 import queue
 import json
 import logging
+import functools
 
 import paho.mqtt.client as mqtt
 
 
-def _make_message_handler(actions):
-    """Create message handler"""
+def _on_message(actions, _client, _obj, msg):
+    """Handle incoming messages from MQTT"""
+    tokens = msg.topic.split("/")
 
-    def _on_message(_client, _obj, msg):
-        tokens = msg.topic.split("/")
+    # Receive and decode, publish in queue
+    action_type = tokens[-1]
+    try:
+        action_payload = json.loads(str(msg.payload, 'utf-8'))
+    except:
+        action_payload = None
 
-        # Receive and decode, publish in queue
-        action_type = tokens[-1]
-        try:
-            action_payload = json.loads(str(msg.payload, 'utf-8'))
-        except:
-            action_payload = None
+    action = {
+        "type": action_type,
+        "payload": action_payload,
+    }
 
-        action = {
-            "type": action_type,
-            "payload": action_payload,
-        }
-
-        actions.put(action)
-
-    return _on_message
+    actions.put(action)
 
 
-def _make_dispatch(client, base_topic):
-    """Create dispatch function"""
-    def dispatch(action):
-        payload = json.dumps(action.get("payload")).encode("utf-8")
-        topic = base_topic + "/" + action["type"]
+def _dispatch(client, base_topic, action):
+    """General action dispatch function"""
+    payload = json.dumps(action.get("payload")).encode("utf-8")
+    topic = base_topic + "/" + action["type"]
 
-        ticket = client.publish(topic, payload)
-        ticket.wait_for_publish()
-
-
-    return dispatch
+    ticket = client.publish(topic, payload)
+    ticket.wait_for_publish()
 
 
-def _make_receive(actions):
-    """Create receive action function"""
-    def receive():
-        try:
-            return actions.get(block=False)
-        except queue.Empty:
-            return None
-
-    return receive
+def _receive(actions):
+    """General receive function"""
+    try:
+        return actions.get(block=False)
+    except queue.Empty:
+        return None
 
 
 def connect(address, base_topic):
@@ -69,7 +59,7 @@ def connect(address, base_topic):
     actions_queue = queue.Queue()
 
     client = mqtt.Client()
-    client.on_message = _make_message_handler(actions_queue)
+    client.on_message = functools.partial(_on_message, actions_queue)
     client.connect(host, int(port), 60)
     client.subscribe("{}/#".format(base_topic))
 
@@ -80,10 +70,10 @@ def connect(address, base_topic):
     client.loop_start()
 
     # Make receive function
-    receive = _make_receive(actions_queue)
+    receive = functools.partial(_receive, actions_queue)
 
     # Create dispatch function
-    dispatch = _make_dispatch(client, base_topic)
+    dispatch = functools.partial(_dispatch, client, base_topic)
 
     return receive, dispatch
 
